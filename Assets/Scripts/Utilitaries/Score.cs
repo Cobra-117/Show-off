3af7a627fc26d31da15e037591b2c7676824b80c
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-05-17 13:48:09 +0200

    add score system

--- Assets/Scripts/Utilitaries/Score.cs
+++ Assets/Scripts/Utilitaries/Score.cs
@@ -2,19 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Score : MonoBehaviour
+public class Players : MonoBehaviour
 {
-    public class PlayerScore {
+    public class PlayerInfos {
         public string name;
         public int score;
         public bool isAlive = true;
+
+        public PlayerInfos(string name) {}
     }
 
-    List<PlayerScore> players;
+    public static List<PlayerInfos> players;
+    static bool HasInit = false;
     // Start is called before the first frame update
     void Awake()
     {
-        players = new List<PlayerScore>();
+        if (HasInit == false) {
+            players = new List<PlayerInfos>();
+            players.Add(new PlayerInfos("p1"));
+            players.Add(new PlayerInfos("p2"));
+            players.Add(new PlayerInfos("p3"));
+            players.Add(new PlayerInfos("p4"));
+            HasInit = true;
+        }
     }
 
     // Update is called once per frame
