9dc54040e2ca2bd6d20d4a185cd67d27d1883efd
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-05-19 12:17:16 +0200

    add moving walls script

--- Assets/Scripts/Walk tile props/AlternatingWall.cs
+++ Assets/Scripts/Walk tile props/AlternatingWall.cs
@@ -4,19 +4,43 @@ using UnityEngine;
 
 public class AlternatingWall : MonoBehaviour
 {
-    public GameObject[] walls;
-    public float changeTime = 2000;
+    List<GameObject> walls;
+    public float changeTime = 2;
 
-    private float countdown;
+   public float countdown;
 
     void Start()
     {
+        walls = new List<GameObject>();
+        //foreach GameObject wall in get
+        for (int i = 0; i < transform.childCount; i++) 
+        {
+            walls.Add(transform.GetChild(i).gameObject);
+        }
         countdown = changeTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        countdown -= Time.deltaTime;
+        if (countdown <= 0) {
+            countdown = changeTime;
+            SwitchWalls();
+        }
+    }
+
+    void SwitchWalls()
+    {
+        for (int i = 0; i < walls.Count; i++) {
+            walls[i].SetActive(true);
+        }
+        int r1 = Random.Range(0, walls.Count);
+        int r2 = r1;
+        while (r1 == r2) {
+            r2 = Random.Range(0, walls.Count);
+        }
+        walls[r1].SetActive(false);
+        walls[r2].SetActive(false);
     }
 }
