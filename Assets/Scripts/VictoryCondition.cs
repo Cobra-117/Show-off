870eb58988e6e990cb1264e299fe42e793f3a658
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-06-01 11:56:29 +0200

    add win and loose conditions

---
+++ Assets/Scripts/VictoryCondition.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VictoryCondition : MonoBehaviour
+{
+    bool hasGameStarted = false;
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        GameObject[] playersobj = GameObject.FindGameObjectsWithTag("Player");
+        List<GameObject> activePlayers = new List<GameObject>();
+
+        for (int i = 0; i < playersobj.Length; i++) {
+            if (playersobj[i].activeInHierarchy)
+                activePlayers.Add(playersobj[i]);
+        }
+        if (activePlayers.Count != 0)
+            hasGameStarted = true;
+        if (activePlayers.Count == 0 && hasGameStarted == true)
+            return;
+    }
+}
