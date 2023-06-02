df1bdc2a7172abbe4993d634f4a2f14cb7e69461
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-06-02 16:27:11 +0200

    set camera settings to follow group target

---
+++ Assets/Scripts/AimPlayers.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimPlayers : MonoBehaviour
+{
+    Cinemachine.CinemachineTargetGroup targetGroup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //targetGroup.AddMember()
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
