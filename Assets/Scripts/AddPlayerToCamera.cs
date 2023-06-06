b3fa914c8b601fc16aacc35c3da49776367483ac
Cobra-117 <timothe.lacheroy@epitech.eu>
2023-06-06 14:51:43 +0200

    fix adding player to camera

--- Assets/Scripts/AddPlayerToCamera.cs
+++ Assets/Scripts/AddPlayerToCamera.cs
@@ -1,26 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AddPlayerToCamera : MonoBehaviour
 {
     GameObject cMCamera;
+    Cinemachine.CinemachineTargetGroup targetGroup;
+    bool hasInit = false;
+    bool isPlayscene = false;
     // Start is called before the first frame update
     void Start()
     {
-        cMCamera = GameObject.FindGameObjectWithTag("TargGroup");
-        Cinemachine.CinemachineTargetGroup targetGroup =
-        cMCamera.GetComponent<Cinemachine.CinemachineTargetGroup>();
-        //cMCamera.GetComponent<Cine>
-        for (int i = 0; i < transform.childCount;i ++) {
-            if (gameObject.activeInHierarchy && tag == "Player") {
-                targetGroup.AddMember(transform, 1, 0);
-            }
-        }
     }
 
+    void OnEnable()
+    {
+        Debug.Log("test");
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
     void Update()
     {
-
+        Debug.Log("child count: " + transform.childCount.ToString());
+        if (!isPlayscene || hasInit)
+            return;
+        for (int i = 0; i < transform.childCount;i ++) {
+            Debug.Log("Tag: " + transform.GetChild(i).gameObject.tag);
+            if (transform.GetChild(i).gameObject.activeInHierarchy && transform.GetChild(i).gameObject.tag == "Player") {
+                targetGroup.AddMember(transform.GetChild(i), 1, 0);
+                Debug.Log("added player");
+            }
+        }
+        hasInit = true;
     }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+        if (scene.name != "PlayScene")
+            return;
+        Debug.Log("Is play scene");
+        cMCamera = GameObject.FindGameObjectWithTag("TargGroup");
+        targetGroup =
+        cMCamera.GetComponent<Cinemachine.CinemachineTargetGroup>();
+        //cMCamera.GetComponent<Cine>
+        Debug.Log("child count: " + transform.childCount.ToString());
+        isPlayscene = true;
+    }
+
 }
