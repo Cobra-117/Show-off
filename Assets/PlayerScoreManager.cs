09769beaefe7356b68c46e7be39b7f3d314ec8f9
AkuwuTumapel <sathyasatoto@gmail.com>
2023-06-12 11:24:01 +0200

    Added ingame UI visuals to PlayScene

--- Assets/PlayerScoreManager.cs
+++ Assets/PlayerScoreManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerScoreManager : MonoBehaviour
 {
     public GameObject[] players;
     public GameObject playerScore;
+    public GameObject iconContainer;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,12 +16,17 @@ public class PlayerScoreManager : MonoBehaviour
         {
             GameObject scorePrefab = Instantiate(playerScore);
             scorePrefab.transform.parent = this.transform;
-            Transform iconContainer = players[i].transform.Find("PlayerIcon");
-            Transform scoreIcon = Instantiate(iconContainer);
-            scoreIcon.transform.parent = this.transform;
+            Transform iconContainer = scorePrefab.transform.Find("Border + Icon");
+            Transform border = iconContainer.transform.Find("Border");
+            Transform icon = iconContainer.transform.Find("Icon");
 
-            
-            
+            GameObject playerManager = GameObject.Find("PlayerManager");
+
+            border.GetComponent<Image>().sprite = playerManager.GetComponent<PlayerSpawnManager>().colors[players[i].GetComponent<PlayerDetails>().playerColor];
+            icon.GetComponent<Image>().sprite = playerManager.GetComponent<PlayerSpawnManager>().icons[players[i].GetComponent<PlayerDetails>().playerIcon];
+
+       
+
             //int color = players[i].GetComponent<PlayerDetails>().playerID-1;
             //GameObject playerColor = Instantiate(colorSprites[color]);
 
