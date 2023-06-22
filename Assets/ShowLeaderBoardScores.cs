344e6ad50d80b306b4f3783c088469a3e11d6e60
AkuwuTumapel <sathyasatoto@gmail.com>
2023-06-22 11:32:24 +0200

    Show leaderboard in end screen

---
+++ Assets/ShowLeaderBoardScores.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowLeaderBoardScores : MonoBehaviour
+{
+    List <PlayerInformation> p;
+    public GameObject scoreList;
+
+    void Start()
+    {
+        p = new();
+    }
+
+    public void ShowScores()
+    {
+        p = ScoreboardXML.Instance.LoadScore();
+        p.Sort();
+        p.Reverse();
+        scoreList.GetComponent<TextMeshProUGUI>().text = "High Scores:\n";
+        foreach (PlayerInformation pl in p)
+        {
+            string s = new string(pl.playerName + " " + pl.playerScore + "\n");
+            scoreList.GetComponent<TextMeshProUGUI>().text += s;
+        }
+        
+    }
+     
+
+}
